e69100953430d4c854d9897ef05df4237a6145a8
melodymutai-source <chepngenomutai99@gmail.com>
2026-05-14 15:22:52 +0300

    added a function for add to cart in my js

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
     buyButtons.forEach(btn => {
         btn.addEventListener('click', handleAddToCart);
     });
+});
+
+function handleAddToCart(event) {
+    const card = event.target.closest('.product-card');
+    const name = card.querySelector('h3').innerText;
+    const priceText = card.querySelector('.price').innerText;
+    
+
